d3dd65a01e5295c06564cbdab2db71f4bd79409d
malbinola <malbinola@sonicle.com>
2026-08-07 16:29:15 +0200

    Update getShareConfigurations

--- src/main/java/com/sonicle/webtop/core/dal/RolePermissionDAO.java
+++ src/main/java/com/sonicle/webtop/core/dal/RolePermissionDAO.java
@@ -171,19 +171,21 @@ public class RolePermissionDAO extends BaseDAO {
 			.fetchInto(ORolePermission.class);
 	}
 	
-	public Map<String, List<ORolePermission>> mapByServiceKeysInstanceRoles(Connection con, String serviceId, Collection<String> keys, String instance, Collection<String> roleSids) throws DAOException {
+	public Map<String, List<ORolePermission>> mapByServiceKeysInstanceUids(Connection con, String serviceId, Collection<String> keys, String instance, Collection<String> subjectUids) throws DAOException {
 		DSLContext dsl = getDSL(con);
 		
-		Condition rolesInCndt = DSL.trueCondition();
-		if (roleSids != null) {
-			rolesInCndt = ROLES_PERMISSIONS.ROLE_UID.in(roleSids);
+		// ROLE_UID -> SUBJECT_UID
+		
+		Condition subjectUidInCndt = DSL.trueCondition();
+		if (subjectUids != null) {
+			subjectUidInCndt = ROLES_PERMISSIONS.ROLE_UID.in(subjectUids);
 		}
 		
 		return dsl
 			.select()
 			.from(ROLES_PERMISSIONS)
 			.where(
-				rolesInCndt
+				subjectUidInCndt
 				.and(ROLES_PERMISSIONS.SERVICE_ID.equal(serviceId))
 				.and(ROLES_PERMISSIONS.KEY.in(keys))
 				.and(ROLES_PERMISSIONS.INSTANCE.equal(instance))
